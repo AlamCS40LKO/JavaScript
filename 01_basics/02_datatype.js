4c79fec0e33ebb76b8f17dbb04865114a3059382
SHANE ALAM <94216032+AlamCS40LKO@users.noreply.github.com>
2025-10-07 07:49:31 +0000

    lecture datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as a newer version
+
+//alert(3+3) give error bcs we use nodejs engine, not browser engine
+
+
+let name = "Alam"
+let age = 23
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
